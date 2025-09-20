aba0144e54c13fbbe4fa8407935497148c361672
+ <+>
2025-09-20 16:33:18 +0300

    solution

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  // game field is 400px wide, dodger is 40px wide → max left = 360
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
